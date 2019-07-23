fd6d8dd67098897b4f6d2467a920a61e1b39337b
Evgeniya88 <Paladina.EM@kvt.su>
2019-07-23 16:00:51 +0300

    Добавлен функционал банера

--- js/init.js
+++ js/init.js
@@ -2,6 +2,8 @@
 var caruselEl = document.querySelector('.banner__content_js').children;
 var arrCarusel = Array.prototype.slice.call(caruselEl); 
 var paginatorEl = document.querySelector('.paginator_js');
+var prevBtnEl = document.querySelector('.banner__nav--left');
+var nextBtnEl = document.querySelector('.banner__nav--right');
 var paginatorArrEl ;
 var arrPaginator = [];
 var arrButtons = [];
@@ -16,17 +18,45 @@ function setHidden(arr) {
 function noActiv(arr) {
 	for (var i=0; i< arr.lenght; i++) {
 		arr[i].classList.remove('paginator__btn--active');
+		console.log('work');
 	}
 }
 
-function NextSlide(index) {
-
+function nextSlide() {
+	if (index < arrCarusel.length-1) {
+		index++;		
+	}
+	else {
+		index = 0;
+	}
+	arrButtons[index].classList.add('paginator__btn--active');
+	arrCarusel[index].classList.remove('visually-hidden');
+	for (var i=0; i< arrButtons.length; i++ ) {
+		if (i != index) {
+			arrButtons[i].classList.remove('paginator__btn--active');
+			arrCarusel[i].classList.add('visually-hidden');
+		}
+	}
 }
 
-function PrevSlide(index) {
-
+function prevSlide() {
+	if (index > 0) {
+		index--;		
+	}
+	else {
+		index = arrCarusel.length-1;
+	}
+	arrButtons[index].classList.add('paginator__btn--active');
+	arrCarusel[index].classList.remove('visually-hidden');
+	for (var i=0; i< arrButtons.length; i++ ) {
+		if (i != index) {
+			arrButtons[i].classList.remove('paginator__btn--active');
+			arrCarusel[i].classList.add('visually-hidden');
+		}
+	}
 }
 
+//создание точек для пагинатора
 for (var i=0; i<arrCarusel.length; i++) {	
 	var paginatorItemEl = document.createElement('li');
 	paginatorItemEl.classList.add('paginator__item');
@@ -35,23 +65,48 @@ for (var i=0; i<arrCarusel.length; i++) {
 	arrPaginator.push(paginatorItemEl);
 }
 
+//
 paginatorArrEl = document.querySelectorAll('.paginator__btn');
-arrPaginator[index].children[0].classList.add('paginator__btn--active');
+paginatorArrEl[index].classList.add('paginator__btn--active');
 arrCarusel[index].classList.remove('visually-hidden');
 
 
 for (var i=0; i<arrCarusel.length; i++) {
-	arrButtons.push(paginatorArrEl[i]);
-	paginatorArrEl[i].addEventListener('click', function(e){
-    	index = arrButtons.indexOf(e.target);
-    	arrCarusel[index].classList.remove('visually-hidden');
-    	arrButtons[index].classList.add('paginator__btn--active');
-	});
+ 	arrButtons.push(paginatorArrEl[i]);
 }
 
+nextBtnEl.addEventListener('click', function(event) {
+	nextSlide();
+});
 
+prevBtnEl.addEventListener('click', function(event) {
+	prevSlide();
+	console.log('work');
+});
 
-console.log(arrCarusel);
+paginatorEl.addEventListener('click', function(event) {
+	if (event.target.tagName == 'BUTTON') {
+		
+		event.target.classList.add('paginator__btn--active');
+
+		index = arrButtons.indexOf(event.target);
+		arrCarusel[index].classList.remove('visually-hidden');
+		for (var i=0; i< arrButtons.length; i++ ) {
+			if (i != index) {
+				arrButtons[i].classList.remove('paginator__btn--active');
+				arrCarusel[i].classList.add('visually-hidden');
+			}
+		}
+		
+	}
+});	
+
+// listTaskEl.addEventListener('click', function(event) {
+// 	if (event.target.tagName == 'LI') {
+// 		event.target.classList.toggle('trought');
+// 	}
+// });	
+
 
 // var res = document.querySelector('.res');
 // var arrElem = document.querySelectorAll('.div');
